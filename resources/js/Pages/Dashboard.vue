0984e92f0867fbbfcdffd27f179dcfab03a2a699
minkokolinn <minkokolinndev@gmail.com>
2025-04-19 00:50:23 +0700

    inertia project setup completed

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,8 @@
+<template>
+  <Head title=" | Dashboard "></Head>
+  <h1>This is Dashboard Page</h1>
+  <p>Lorem ipsum</p>
+</template>
+<script setup>
+
+</script>
---
+++ resources/js/app.js
@@ -0,0 +1,18 @@
+import { createApp, h } from "vue";
+import { createInertiaApp, Head, Link } from "@inertiajs/vue3";
+
+createInertiaApp({
+    title:(title)=>`QuizCraft ${title}`,
+    resolve: name=>{
+        const pages = import.meta.glob('./Pages/**/*.vue',{eager:true});
+        let page = pages[`./Pages/${name}.vue`];
+        return page;
+    },
+    setup({el,App,props,plugin}){
+        createApp({render:()=>h(App,props)})
+        .use(plugin)
+        .component("Head",Head)
+        .component("Link",Link)
+        .mount(el)
+    }
+});
